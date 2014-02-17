51a2765bf6150561717c64df2ed9662e9563306d
Keon Vafai <keylime503@gmail.com>
2014-02-16 23:00:12 -0800

    Added check for wrong number of parameters

--- sms.sh
+++ sms.sh
@@ -3,4 +3,8 @@
 # First argument is 10-digit phone number
 # Second argument is string message to be sent
 
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters. Usage: ./sms.sh <phone_number> <string_message>"
+fi
+
 curl "http://textbelt.com/text" -d number="$1" -d "message=$2"
